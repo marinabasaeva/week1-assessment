ae7d05d43e663594a124b75afd13eeaf9e758734
Marina Basaeva <basaeva.marina@gmail.com>
2022-05-06 11:31:30 -0400

    initial commit

---
+++ gitInfo.js
@@ -0,0 +1,80 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+//CODE HERE
+var gitDefinition = "a free and open source distributed version control system"
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+//CODE HERE
+var gitHubDefinition = "a code hosting platform for version control and collaboration"
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.  
+    It should be a string containing your best definition of what 'git init' does.
+*/
+//CODE HERE
+var gitInitDefinition = "creates a new Git repository"
+
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.  
+    It should be a string containing your best definition of what 'git clone' does.
+*/
+//CODE HERE
+var gitCloneDefinition = "creates a copy of a specific repository or branch within a repository"
+
+//////////////////PROBLEM 5////////////////////
+/*
+    Create a variable called 'gitStatusDefinition'.  
+    It should be a string containing your best definition of what 'git status' does.
+*/
+//CODE HERE
+var gitStatusDefinition = "displays the state of the repository and staging area"
+
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.  
+    It should be a string containing your best definition of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.  
+    It should be a string containing the code to add all files.
+*/
+//CODE HERE
+var gitAddDefinition = "adds new or changed files in your working directory to the Git staging area"
+var gitAddCode = "add ."
+
+//////////////////PROBLEM 7////////////////////
+/*
+    Create a variable called 'gitCommitDefinition'.  
+    It should be a string containing your best definition of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.  
+    It should be a string containing the code to commit using the message "initial commit".
+*/
+//CODE HERE
+var gitCommitDefinition = "saves your changes to the local repository"
+var gitCommitCode = "git commi -m 'your message'"
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create a variable called 'gitPushDefinition'.  
+    It should be a string containing your best definition of what 'git push' does.
+*/
+//CODE HERE
+var gitPushDefinition = "updates the remote branch with local commits"
+
+
+
+
---
+++ sldc.js
@@ -0,0 +1,33 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+/*
+1. Planning
+2. Analysis of Requirements
+3. Design
+4. Implementation
+5. Testing & Integration
+6. Maintain
+
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = "the process of determining user expectations for a new or modified product."
+const design = "this phase would include determining the priority order of the proposed work, building a product roadmap, and obtaining stakeholder agreement on it. This will help everyone on both the development and product teams get a clearer picture of their aims."
+const implementation = "This is the stage where the engineering team actually codes the product. At this stage, the development team translates the high-level overview communicated in the roadmap into a tactical set of assignments, due dates, and day-to-day work schedules."
+const testingAndIntegration = "the QA team and the developers will test all areas of the application to spot any defects, bugs, or other problems. Then, the team is confident it has fixed all defects and that the software has been built to the agreed-upon goals and specifications. This means the product will be generally available for customers to buy and use."
+const maintain = "The developers will need to be ready to address requests for enhancements, bug fixes, and new features. "
+
+
+
+
+
